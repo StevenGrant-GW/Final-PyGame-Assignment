121f67c6e3ba1d1d8982fc6c92eeec776197887b
StevenGrant-GW <1loganzan@hdsb.ca>
2025-12-19 08:47:16 -0500

    Adds player character and player movement

--- assignment.py
+++ assignment.py
@@ -49,10 +49,16 @@ pygame.init()
 
 # *********SETUP**********
 
-windowWidth = 500
-windowHeight = 500
+windowWidth = 800
+windowHeight = 600
 window = pygame.display.set_mode((windowWidth, windowHeight))
 clock = pygame.time.Clock()  #will allow us to set framerate
+
+# Variables for setup
+player_x = 250
+player_y = 250
+player_speed = 5
+# ---------------------------
 
 # *********GAME LOOP**********
 while True:
@@ -60,14 +66,29 @@ while True:
     ev = pygame.event.poll()    # Look for any event
     if ev.type == pygame.QUIT:  # windowow close button clicked?
         break                   #   ... leave game loop
-   
+    
     #PUT YOUR MOUSE/KEYBOARD EVENTS HERE
-   
+    
     # *********GAME LOGIC**********
-   
+    
+    # Movement Logic here to cause the character to move each cardinal direction with WASD
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        player_x -= player_speed
+    if keys[pygame.K_d]:
+        player_x += player_speed
+    if keys[pygame.K_w]:
+        player_y -= player_speed
+    if keys[pygame.K_s]:
+        player_y += player_speed
+    
     #PUT YOUR GAME LOGIN HERE FOR EACH GAMESTATE
-   
+    
     # *********DRAW THE FRAME**********
+    window.fill((0, 0, 0)) # Clears the screen with black to keep the character from leaving a trail
+
+    # Draws the player character as a red cube
+    pygame.draw.rect(window, (255, 0, 0), (player_x, player_y, 20, 20))
 
     #PUT YOUR DRAWING, IMAGE PLACEMENT, BLIT ETC.. COMMANDS HERE FOR EACH GAMESTATE'''
 
