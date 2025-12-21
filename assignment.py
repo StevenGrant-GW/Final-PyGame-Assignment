2042f7d1f801314c3df74fcb494cdfcad56e5bbe
StevenGrant-GW <1loganzan@hdsb.ca>
2025-12-20 22:12:15 -0500

    Added custom sounds/images and font along with improving menu's
    added sounds for different actions
    added background image
    added custom font replacing the old arial
    improved menu instructions to add how to play
    
    finished reflections and written work at top

--- assignment.py
+++ assignment.py
@@ -5,8 +5,9 @@ Program Description: Move a red square to collect green targets while avoiding
 an orange bouncing obstacle. Features a start menu and score system an end screen.
 -----------------------------------------------------------------------------
 References:
-https://github.com/tczkqq/dvd-corner/blob/master/dvd-corner.py
-https://www.pygame.org/docs/ref/rect.html
+https://github.com/tczkqq/dvd-corner/blob/master/dvd-corner.py (used to help me make the obstacle)
+https://www.pygame.org/docs/ref/rect.html (used in how i made collisions)
+https://realpython.com/python-f-strings/ (taught me how to use f-strings to make the score system)
 (put a link to your reference here but also add a comment in the code below where you used the reference)
 
 
@@ -16,12 +17,18 @@ https://www.pygame.org/docs/ref/rect.html
 Additional Libraries/Extensions:
 None required (standard Pygame)
 
+Additional files to install in folder:
+background.jpg
+Blip9.WAV
+Hit4.WAV
+Pickup9.WAV
+PressStart2P-Regular.ttf
 
 -----------------------------------------------------------------------------
 
 
 Known bugs:
-None
+1. due to the obstacle starting where the game last ended it can sometimes spawn inside the player
 
 
 ----------------------------------------------------------------------------
@@ -30,36 +37,37 @@ None
 
 
 Program Reflection:
-I think this project deserves a level XXXXXX because ...
-
+I think this project deserves a level 3+ because it manages to fulfill nearly every criteria
+and uses some more complex mechanics like multiple menus/states and multiple external assets while
+making a replayable game program
 
  Level 3 Requirements Met:
-•
-•  
-•  
-•  
-•  
-•
-
-
-Features Added Beyond Level 3 Requirements:
-•
-•  
-•  
-•  
-•  
-•
------------------------------------------------------------------------------
+• Window size fits criteria
+• Several user events including starting game, movement with WASD, restarting and ending game
+• Using pygame.key.get_pressed() prevents issues from invalid keys
+• Uses Integers (pos/size) and Strings (game_state)
+• uses if/elif extensively throughout the program
+• the main game runs of a While running: loop
+• reset_game() is a custom function
+• game has multiple screens with Menu, Game and Game over
+• game has instructions displayed for every menu
+• game includes several sounds for different actions
+• game loads and scales a background
+• game uses .colliderect() and .collidepoint() to make collision
+• game loads PressStart2P-Regular.ttf as a font for all menus
 '''
 
 
 import pygame
 import random
+import os
 pygame.init()
 
+#finds the location of the folder where the game is
+current_path = os.path.dirname(__file__)
+os.chdir(current_path)
 
 # *********SETUP**********
-
 
 windowWidth = 800
 windowHeight = 600
@@ -67,6 +75,13 @@ window = pygame.display.set_mode((windowWidth, windowHeight))
 pygame.display.set_caption("Square Collector")
 clock = pygame.time.Clock()  #will allow us to set framerate
 
+# *********LOAD ASSETTS*********
+coin_pickup = pygame.mixer.Sound("Pickup9.WAV")
+obstacle_hit = pygame.mixer.Sound("Hit4.WAV")
+game_start = pygame.mixer.Sound("Blip9.WAV")
+font_file = "PressStart2P-Regular.ttf"
+bg_img = pygame.image.load("background.jpg")
+bg_img = pygame.transform.scale(bg_img, (windowWidth, windowHeight))
 
 # Variables for the player character
 player_x = 385 # Centered for 800 width
@@ -84,9 +99,9 @@ target_size = 20
 # Variables to set up a score system
 score = 0
 # Using default fonts to ensure consistency across different computers
-font = pygame.font.SysFont("Arial", 32)
-title_font = pygame.font.SysFont("Arial", 72, bold=True)
-
+font = pygame.font.Font(font_file, 15)
+title_font = pygame.font.Font(font_file, 40)
+small_font = pygame.font.Font(font_file, 20)
 
 # Variables for Obstacle
 obs_radius = 25
@@ -104,6 +119,13 @@ game_state = "MENU"
 # *********GAME LOOP**********
 running = True
 while running:
+
+    #set up custom function to handle Game Over
+    def reset_game(): 
+        global score, game_state, player_x, player_y, obs_x_speed, obs_y_speed
+        score, game_state, player_x, player_y = 0, "GAME", 385, 285
+        obs_x_speed, obs_y_speed = 4, 4
+
     # *********EVENTS**********
     ev = pygame.event.poll()    # Look for any event
     if ev.type == pygame.QUIT:  # window close button clicked?
@@ -114,6 +136,7 @@ while running:
    
     if game_state == "MENU":
         if keys[pygame.K_SPACE]:
+            game_start.play()
             game_state = "GAME"
 
 
@@ -129,7 +152,7 @@ while running:
 
     elif game_state == "GAMEOVER":
         if keys[pygame.K_r]:
-            score, game_state, player_x, player_y = 0, "GAME", 385, 285
+            reset_game()
         if keys[pygame.K_ESCAPE]:
             running = False
 
@@ -165,6 +188,7 @@ while running:
         # Checks if the player touches the target to increase score and move the target
         if player_rect.colliderect(target_rect):
             score += 1
+            coin_pickup.play() # play coin pickup sound
             target_x = random.randint(0, windowWidth - target_size)
             target_y = random.randint(0, windowHeight - target_size)
 
@@ -178,6 +202,7 @@ while running:
 
         # Checks if the player touches the obstacle to trigger a game over
         if player_rect.collidepoint(obs_x, obs_y):
+            obstacle_hit.play() # play coin pickup sound
             game_state = "GAMEOVER"
    
     # *********DRAW THE FRAME**********
@@ -187,20 +212,30 @@ while running:
     if game_state == "MENU":
         # Render surfaces
         title_surf = title_font.render("SQUARE COLLECTOR", True, (255, 255, 255))
-        instr_surf = font.render("Touch the green square for points and avoid the Orange Circle!", True, (255, 255, 255))
+        instr1_surf = small_font.render("TOUCH THE GREEN SQUARE FOR POINTS", True, (255, 255, 255))
+        instr2_surf = small_font.render("AND AVOID THE ORANGE CIRCLE!", True, (255, 255, 255))
+        instr3_surf = font.render("Press WASD to move", True, (200, 200, 200))
         start_surf = font.render("Press SPACE to Start", True, (200, 200, 200))
        
         # Draw centered text by calculating (Center - Half Width)
         title_pos = (windowWidth // 2 - title_surf.get_width() // 2, windowHeight // 4)
-        instr_pos = (windowWidth // 2 - instr_surf.get_width() // 2, windowHeight // 2 - 20)
-        start_pos = (windowWidth // 2 - start_surf.get_width() // 2, windowHeight // 2 + 60)
+        instr1_pos = (windowWidth // 2 - instr1_surf.get_width() // 2, windowHeight // 2 - 50)
+        instr2_pos = (windowWidth // 2 - instr2_surf.get_width() // 2, windowHeight // 2 - 20)
+        instr3_pos = (windowWidth // 2 - instr3_surf.get_width() // 2, windowHeight // 2 + 60)
+        start_pos = (windowWidth // 2 - start_surf.get_width() // 2, windowHeight // 2 + 80)
        
+       # pairs the different texts and there positions/sizes
         window.blit(title_surf, title_pos)
-        window.blit(instr_surf, instr_pos)
+        window.blit(instr1_surf, instr1_pos)
+        window.blit(instr2_surf, instr2_pos)
+        window.blit(instr3_surf, instr3_pos)
         window.blit(start_surf, start_pos)
 
 
     elif game_state == "GAME":
+        #Draw the background image
+        window.blit(bg_img, (0, 0))
+
         # Draw the target green square
         pygame.draw.rect(window, (0, 255, 0), (target_x, target_y, target_size, target_size))
        
@@ -219,7 +254,7 @@ while running:
     elif game_state == "GAMEOVER":
         # Render surfaces
         over_surf = title_font.render("GAME OVER", True, (255, 0, 0))
-        final_score_surf = font.render(f"Final Score: {score}", True, (255, 255, 255))
+        final_score_surf = title_font.render(f"Final Score: {score}", True, (255, 255, 255))
         restart_surf = font.render("Press R to Restart", True, (200, 200, 200))
         quit_surf = font.render("Press Escape to Quit", True, (150, 150, 150))
 
